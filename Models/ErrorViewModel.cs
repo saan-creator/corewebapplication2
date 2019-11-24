e81b823429187c01e8e460aaa69af4f68202f51a
Shan <Shan@SHAN-PC.com>
2019-11-24 20:28:49 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcoreWebApplication2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
